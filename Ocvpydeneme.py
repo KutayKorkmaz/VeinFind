7c3e72bd713fe24c0d7e82554e60011e13ed87ce
KutayKorkmaz <kuty.korkmaz@hotmail.com>
2017-03-24 13:07:27 +0000

    16:07

--- Ocvpydeneme.py
+++ Ocvpydeneme.py
@@ -11,7 +11,7 @@ camera.capture('denemedamar.png')
 sleep(0.042)
 src=cv2.imread('denemedamar.png')
 tmp=cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
-_,alpha=cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
+_,alpha=cv2.threshold(tmp,10,255,cv2.THRESH_BINARY)
 b,g,r=cv2.split(src)
 rgba=[b,g,r,alpha]
 dst=cv2.merge(rgba,4)
